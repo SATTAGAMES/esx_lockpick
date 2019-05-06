2610887bbb3d960c4f8e382afa6dd847938ad7a3
SATTAGAMES <48390215+SATTAGAMES@users.noreply.github.com>
2019-05-06 13:41:41 -0300

    add br.lua
    Added portuguese language

---
+++ locales/br.lua
@@ -0,0 +1,12 @@
+Locales ['br'] = {
+	['used_kit']					= 'Você usou x1 lockpick.',
+	['must_be_outside']				= 'Você deve estar fora do veículo!',
+	['no_vehicle_nearby']			= 'Não há veículo nas proximidades.',
+	['vehicle_unlocked']			= 'Você destrancou o veículo!',
+	['abort_hint']					= 'Pressione ~INPUTGROUP_MOVE~ Para cancelar arrombamento..',
+	['aborted_lockpicking']		    = 'Você cancelou o arrombamento.',
+	['picklock_failed']		        = 'Droga, a chave emperrou.',
+	['911Call']		                = '190 Roubo em andamento.',
+	['911Lockpick']		            = 'Um sistema ant-roubo de veiculo foi acionado',
+	['Lockcall']		            = 'Alguém está tentando arrombar um veículo. Anexei uma foto da pessoa.',
+}
